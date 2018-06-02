6f275e8485d352bf230bfe15bc1a7badb4f9b0e3
corytveten <corytveten@gmail.com>
2018-06-02 13:44:23 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,14 +1,21 @@
 
 
 module Memorable
+  module ClassMethods
 
-  def reset_all
-    self.all.clear
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
   end
 
-  def count
-    self.all.count
-  end
+  module InstanceMethods
+    def initialize
 
+    end
+  end
 
 end
